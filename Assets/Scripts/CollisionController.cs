591b782ecb50ae70b30378f1380173f3ffdd67ff
KhanGeek <han1995.95@mail.ru>
2026-07-02 14:32:22 +0400

    задание выполнено

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -1,20 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CollisionController : MonoBehaviour
 {
     [SerializeField] private EnemyController _enemyController;
-    
-    private void OnCollisionStay(Collision collision)
+    [SerializeField] private SphereCollider _collider;
+
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.gameObject.TryGetComponent(out PlayerInput playerInput))
+        if (other.gameObject.TryGetComponent(out PlayerInput playerInput))
             _enemyController.ReactActive(playerInput.transform);
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.TryGetComponent(out PlayerInput playerInput))
+        if (other.gameObject.TryGetComponent(out PlayerInput playerInput))
             _enemyController.ReactDisabled();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _collider.radius);
     }
 }
